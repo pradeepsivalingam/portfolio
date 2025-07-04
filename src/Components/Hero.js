2a366b36f7bda8b1bcbfee04c271eb1f632b16d2
Pradeep S <114345025+pradeepsivalingam@users.noreply.github.com>
2025-07-04 11:45:11 +0530

    Update Hero.js

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
-import HeroImg from '../assets/hero.png';
+// import HeroImg from '../assets/hero.png';
 import About from './About';
-// Importing AOS (Animate On Scroll) library for animations 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Education from './education.js';
@@ -11,7 +10,6 @@ import Profile from './profile.js';
 import Contact from './contact.js';
 import Copyrights from './Copyrights.js';
 
-
 export default function Hero() {
     useEffect(() => {
         AOS.init({ duration: 1000 });
@@ -19,30 +17,24 @@ export default function Hero() {
 
     return (
         <>
-            <section className='flex px-5 py-40 bg-secondary justify-center'>
-                <div
-                    className="flex justify-end items-center h-screen pr-16"
-                    data-aos="fade-right"
-                >
-                    <div className="w-1/2 text-white text-3xl">
-                        Hi, I'm <span className=""></span> PRADEEP<p className="text-xl">I'm a Developer</p>
-                    </div>
-                </div>
-                <img
-                    className='w-8/12'
-                    src={HeroImg}
-                    alt='Hero'
-                    data-aos="fade-left"
-                />
-            </section>
+            <section className="flex items-center justify-center h-screen bg-secondary px-5">
+  <div
+    className="text-white text-center justify-center .hero-title items-center"
+    data-aos="fade-right"
+  >
+    <h1 className="text-3xl hero-title">Hi, I'm PRADEEP</h1>
+    <p className="text-4xl mt-2">I'm a Developer</p>
+  </div>
+</section>
+
+
             <About />
             <Education />
             <Skills />
             <Service />
             <Profile />
-            <Contact/>
+            <Contact />
             <Copyrights />
-
         </>
     );
 }
